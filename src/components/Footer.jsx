d939c7f03f67ac556ecc9735a314ab690731f806
kenedy4 <kennedymusyoki8@gmail.com>
2024-08-14 22:25:55 +0300

    Formatted login pages to have a background image and also appear as a card

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,11 @@ const Footer = () => {
   return (
     <footer>
       <p>&copy; 2024 LetaFood. All rights reserved.</p>
+    <h3>Meet the management</h3>
+    <p>Kenedy Musyoki: Director</p>
+    <p>AnnG Mueni: Marketing Strategist and Product Design</p>
+    <p>RickFalton: CEO</p>
+    <p>Farida Mutai: Human Resources</p>
     </footer>
   );
 };
--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -10,27 +10,29 @@ const Login = ({ handleLogin }) => {
   };
 
   return (
-    <div className="login">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div className="login-container">
+      <div className="login-card">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
